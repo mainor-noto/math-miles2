5d1d65a930846c834252a29de26aa9c064f502d2
mainor martinez <martimai000@warren.k12.in.us>
2024-08-26 10:09:37 -0400

    initial commit

---
+++ math2.py
@@ -0,0 +1,9 @@
+miles = 5
+minutes = 45
+hours = minutes / 60
+
+mph = miles / hours
+
+print("How many miles did you run?", miles)
+print("How many minutes did it take you?", minutes)
+print("speed in mph:", mph)
